6484a9b893a05bf9ebd1482f169e6018cd616777
Shuquan Huang <huang.shuquan@99cloud.net>
2016-09-19 22:02:54 +0800

    Update build-images script and chmod +x bootstrap script

--- build-images.sh
+++ build-images.sh
@@ -1,1 +1,5 @@
-docker build -t openstack/base -f base .
+docker build -t openstack/base base
+docker build -t openstack/openstackclient openstackclient
+docker build -t openstack/keystone keystone
+docker build -t openstack/glance glance
+
