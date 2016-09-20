9f8bf977b20df70fd78a592ab01c142b8da539b4
Shuquan Huang <huang.shuquan@99cloud.net>
2016-09-20 21:28:34 +0800

    Enable nova controller services
    1. set static ip for nova container
    2. set privileged=true due to “permission denied (you must be root)”
    error message of iptable

--- build-images.sh
+++ build-images.sh
@@ -2,4 +2,6 @@ docker build -t openstack/base base
 docker build -t openstack/openstackclient openstackclient
 docker build -t openstack/keystone keystone
 docker build -t openstack/glance glance
-
+docker build -t openstack/nova nova
+docker build -t openstack/neutron neutron
+docker build -t openstack/cinder cinder
